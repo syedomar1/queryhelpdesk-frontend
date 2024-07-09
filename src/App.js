a46229a46c06b3316fb41460fe99dc91d5d68216
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-07-09 17:46:26 +0530

    Updated Routings

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-// import Navbar from "./components/Navbar";
 import Admin from "./components/Admin";
 import UserRaiseTicket from "./components/UserRaiseTicket";
 import UserAllTicket from "./components/UserAllTicket";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Homepage from "./components/Homepage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AllTickets from "./components/AllTickets";
+import { AuthProvider } from "./components/AuthContext";
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/hello`)
+    fetch(`${process.env.REACT_APP_BACKEND_URL}`)
       .then((response) => response.text())
       .then((data) => setMessage(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
-    <div className="App">
-      <div>
+    <AuthProvider>
+      <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Admin />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/userraiseticket" element={<UserRaiseTicket />} />
-            <Route path="/userallticket" element={<UserAllTicket />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+            <Route path="/userraiseticket" element={<ProtectedRoute><UserRaiseTicket /></ProtectedRoute>} />
+            <Route path="/userallticket" element={<ProtectedRoute><UserAllTicket /></ProtectedRoute>} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/allqueries" element={< AllTickets/>}/> 
           </Routes>
         </BrowserRouter>
-        {/* <header className="App-header">
-        <h1>Message from Backend:</h1>
-        <p>{message}</p>
-      </header> 
-        <Navbar />
-        <Admin />
-        <p>{message}</p>*/}
       </div>
-    </div>
+    </AuthProvider>
   );
 }
 
